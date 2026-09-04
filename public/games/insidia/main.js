bb034ba61014952edb1e7289df492d02329778d7
Felipe Budinich <fbudinichd+1@gmail.com>
2026-09-04 13:36:43 -0400

    Increase Insidia game resolution to 1920x1080

--- public/games/insidia/main.js
+++ public/games/insidia/main.js
@@ -2,4 +2,4 @@ import ig from "../../lib/impact/impact.js";
 import "./media/assets.js";
 import { InsidiaGame } from "./game/insidia-game.js";
 ig.Sound.enabled = false;
-ig.main("#canvas", InsidiaGame, 60, 1600, 900, 1);
+ig.main("#canvas", InsidiaGame, 60, 1920, 1080, 1);
